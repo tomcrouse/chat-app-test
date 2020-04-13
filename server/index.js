50d88387f7af0b226501d4451e5d715893ebe332
Tony Kark <tonywebdevkark@gmail.com>
2020-04-13 21:35:16 +0300

    chat test v1.0.1

--- server/index.js
+++ server/index.js
@@ -7,44 +7,45 @@ const moment = require('moment')
 const cors = require('cors')
 const PORT = process.env.PORT || 8080
 const router = require('./router')
-const {
-    addUser,
-    getUser,
-    deleteUser,
-    getRoomUsers
-} = require('./utils/users')
+const { addUser, getUser, deleteUser, getRoomUsers } = require('./utils/users')
 const formatMessage = require('./utils/messages')
 const bot = 'ChatBot'
 
 app.use(router)
 app.use(cors)
 
-
+//listen to on initial connection from client
 io.on('connection', socket => {
 
+    // listen to on request to join room
     socket.on('join-room', ({ user, room }, cb) => {
 
         const { error, newUser } = addUser(socket.id, user, room)
 
+        //handle error if user name is already taken
         if (error) {
             return cb(error)
         } else {
+            //join new user
             socket.join(newUser.room)
+            //send all room users new data about users in room
             io.to(newUser.room).emit('roomUsers', {
                 room: newUser.room,
                 users: getRoomUsers(newUser.room)
             })
+            // send only new user greeting
             socket.emit('chat-message', formatMessage(bot, `Hello, ${newUser.name}! Welcome to room ${newUser.room}!`))
+            // send all users exept new user message about new user
             socket.broadcast.to(newUser.room)
                 .emit('newUser', formatMessage(bot, `User ${newUser.name} has joined the room ${newUser.room}`))
         }
     })
-
+    //listen to on new message in chat
     socket.on('chat-message', msg => {
         const user = getUser(socket.id)
         io.to(user.room).emit('chat-message', formatMessage(user.name, msg))
     })
-
+    //listen to on user disconnection
     socket.on('disconnect', () => {
 
         const user = deleteUser(socket.id)
